716c97b1407560dd3aff30165be704398ada538a
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2019-07-05 17:54:47 +0200

    add new resources with twitter account of organizations

--- getdata.sh
+++ getdata.sh
@@ -3,3 +3,5 @@
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query="$(< organisations.sparql)" -H 'Accept: text/csv' > organisations.csv
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query="$(< siren-datagouv.sparql)" -H 'Accept: text/csv' > siren-datagouv.csv
+
+curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query="$(< twitter.sparql)" -H 'Accept: text/csv' > twitter.csv
--- publish.sh
+++ publish.sh
@@ -10,6 +10,9 @@ export api="https://www.data.gouv.fr/api/1"
 export dataset_id="5d0d24af634f411c05d9ca9b"
 export resource_id_list="6995e10d-2728-4141-b583-d3aa5294712a"
 export resource_id_siren="8ed46317-288c-481c-829a-068f4ceb00ee"
+export resource_id_twitter="538f4021-d834-405f-8a51-c698b70c705f"
+
 export api_key=$API_KEY
 curl "$api/datasets/$dataset_id/resources/$resource_id_list/upload/" -F "file=@organisations.csv" -H "X-API-KEY:$api_key"
 curl "$api/datasets/$dataset_id/resources/$resource_id_siren/upload/" -F "file=@siren-datagouv.csv" -H "X-API-KEY:$api_key"
+curl "$api/datasets/$dataset_id/resources/$resource_id_twitter/upload/" -F "file=@twitter.csv" -H "X-API-KEY:$api_key"
