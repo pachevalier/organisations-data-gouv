7edf9f7d892f1fabf5dea790bf06c30176d1ddd6
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2019-07-05 10:57:45 +0200

    automatically update list of siren number

--- getdata.sh
+++ getdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query="$(< organisations.sparql)" -H 'Accept: text/csv' > organisations.csv
+
+curl -G https://query.wikidata.org/bigdata/namespace/wdq/sparql --data-urlencode query="$(< siren-datagouv.sparql)" -H 'Accept: text/csv' > siren-datagouv.csv
